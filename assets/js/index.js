ecb28d280421621ce5e27642232b1b0b7a944db8
IrynaKr1 <krutsiryna@gmail.com>
2025-08-02 17:11:57 +0200

    Add image model

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,13 +6,47 @@ const RAND_TEXT = [
   'New button text is so long!',
 ];
 
+/**
+ * PageImg class manage default and hover states for image element
+ * @class PageImg
+ */
+
+class PageImg {
+  /**
+   * Creates an instance of PageImg with predefined default and hover images.
+   * @constructor
+   * @memberof PageImg
+   * @property {string} src = URL of the image
+   * @property {string} alt = Alt text for accessibility
+   */
+  constructor() {
+    this.defaultImg = {
+      src: 'https://cdn.pixabay.com/photo/2015/04/22/12/02/butterfly-734654_1280.jpg',
+      alt: 'Butterfly on a grass',
+    };
+    this.hoverImg = {
+      src: 'https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_960_720.jpg',
+      alt: 'Sunset on the beach',
+    };
+  }
+
+  imageHover(imgElement) {
+    imgElement.src = this.hoverImg.src;
+    imgElement.alt = this.hoverImg.alt;
+  }
+  imageToDefault(imgElement) {
+    imgElement.src = this.defaultImg.src;
+    imgElement.alt = this.defaultImg.alt;
+  }
+}
+
 const [textButton, colorButton, allButton, nextButton] =
   document.querySelectorAll('.btn');
 const img = document.querySelector('img');
+const pageImg = new PageImg();
 
-img.src =
-  'https://cdn.pixabay.com/photo/2015/04/22/12/02/butterfly-734654_1280.jpg';
-img.alt = 'Butterfly on a grass';
+img.src = pageImg.defaultImg.src;
+img.alt = pageImg.defaultImg.alt;
 
 function changeText() {
   const randomText = RAND_TEXT[Math.floor(Math.random() * RAND_TEXT.length)];
@@ -32,8 +66,11 @@ function addBothFunctions(button) {
 }
 
 function changeImg() {
-    img.src = 'https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_960_720.jpg';
-    img.alt = 'Sunset on the beach';
+  pageImg.imageHover(img);
+}
+
+function resetImg() {
+  pageImg.imageToDefault(img);
 }
 
 textButton.addEventListener('click', changeText);
@@ -41,3 +78,4 @@ colorButton.addEventListener('click', changeColor);
 allButton.addEventListener('click', addBothFunctions(allButton));
 
 img.addEventListener('mouseenter', changeImg);
+img.addEventListener('mouseleave', resetImg);
